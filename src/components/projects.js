ee9d9977e4f603770c21eb855966d60b96b7a6c3
Danny Doan <dannydoan43@gmail.com>
2022-05-06 01:00:09 -0500

    updating projects
    adding in picture for sigma

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,13 +13,13 @@ export default class Header extends Component {
                     <div>
                         <table className="toolsused">
                         <tr >
-                            <td><img className="pic1" src="potty.png" alt="shit is broken"/></td>
-                            <td className="tablepad"><h1 className="boxcolor">PottyPicker <a href="https://github.com/dannydoan43/PotTYpicker" target="_blank" ><img src="github.png" className="githubpadding"/> </a></h1> <p className="pottytext">is a JavaFX application that makes use of the JavaMail API. Students are able to email janitorial staff which bathrooms are the best/worst. </p></td>
+                            <td><img className="pic1" src="homepage.png" alt="broken"/></td>
+                            <td className="tablepad"><h1 className="boxcolor">PottyPicker <a href="https://github.com/dannydoan43/sigma" target="_blank" ><img src="github.png" className="githubpadding"/> </a></h1> <p className="pottytext">is a JavaFX application that makes use of the JavaMail API. Students are able to email janitorial staff which bathrooms are the best/worst. </p></td>
                         </tr>
                             <br></br><br></br><br></br>
                             <tr className="tablepadding">
-                                <td><img className="digits" src="digitrec.png" alt="here we go again"/></td>
-                                <td className="tablepad"> <h1 className="boxcolor">Digital Recognition <a href="https://github.com" target="_blank" ><img src="github.png" className="githubpadding"/> </a></h1> <p className="pottytext">is a machine learning project done in Python that uses numpy, pandas, keras, tensorflow and matplotlib with the MNIST handwritten digit dataset.</p></td>
+                                <td><img className="postpage.png" src="" alt="here we go again"/></td>
+                                <td className="tablepad"> <h1 className="boxcolor">Digital Recognition <a href="https://github.com/dannydoan43/sigma" target="_blank" ><img src="github.png" className="githubpadding"/> </a></h1> <p className="pottytext">is a machine learning project done in Python that uses numpy, pandas, keras, tensorflow and matplotlib with the MNIST handwritten digit dataset.</p></td>
                             </tr>
                         </table>
                     </div>
